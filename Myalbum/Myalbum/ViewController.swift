00e98e59f7f3fd933755769eb8c0f76e960657d6
pastapeter <pastapeter@naver.com>
2021-01-17 16:29:47 +0900

    ios first commit

---
+++ Myalbum/Myalbum/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Myalbum
+//
+//  Created by abc on 2021/01/17.
+//
+// closure = {} 코드블럭
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var currentValue = 0
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        refresh()
+    }
+
+
+    @IBAction func showAlert(_ sender: Any) {
+        let message = "가격은 \(currentValue) 입니다."
+        let alert = UIAlertController(title:"HELLO", message:message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: {action in
+            self.refresh()
+        })
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func refresh(){
+        let randomPrice = arc4random_uniform(10000)+1
+        currentValue = Int(randomPrice)
+        priceLabel.text = "₩\(currentValue)"
+    }
+    
+}
+
