4ae1c51c3934b00acacf6859cda0ef9e46baeb2f
DOHYUN <pastapeter@naver.com>
2021-07-18 20:43:57 +0900

    feat: Bank의 open 함수, teller에게 일주는 함수 구현 완료

--- BankManager_concurrency/BankManager_concurrency/Bank.swift
+++ BankManager_concurrency/BankManager_concurrency/Bank.swift
@@ -8,22 +8,46 @@
 import Foundation
 
 class Bank {
-    private var clients = [Client]()
-    private var tellers = [Teller]()
+    private var clients : [Client] = []
+    private var tellers : [Teller] = []
     private var finishedClientCount: Int = 0
     private var businessTime: TimeInterval?
     
-    init() {
-        <#statements#>
+    func open(teller: [Teller], client: [Client]) {
+        //teller 은행원수 이미 정해져있음
+        //client
+        let openDate = Date()
+        
+        tellers = teller
+        clients = client.sorted()
+        assignBusinessToTeller()
+        businessTime = Date().timeIntervalSince(openDate)
+        resetFinishedClientCount()
     }
     
-    func open(teller: [Teller], client: [Client]) {
-        
-    }
     private func assignBusinessToTeller(){
-        
+        let windows = DispatchGroup()
+        var clientIndex = clients.count-1
+        var isContinuous = true
+        while isContinuous {
+            for teller in tellers {
+                if clientIndex == 0 {
+                    teller.handleBusiness(client: clients[clientIndex], with: windows)
+                    isContinuous = false
+                    break
+                }
+                if teller.isNotworking {
+                    teller.handleBusiness(client: clients[clientIndex], with: windows)
+                    clientIndex -= 1
+                }
+            }
+        }
+        windows.notify(queue: DispatchQueue.main){
+            print("모든손님의 일처리를 모두 마무리했다!!!")
+        }
     }
     private func resetFinishedClientCount(){
-        
+        finishedClientCount = 0
+        clients.removeAll()
     }
 }
