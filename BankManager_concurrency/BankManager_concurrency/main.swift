937ce4b9ced5812960dc4776198a753bb8c4f441
DOHYUN <pastapeter@naver.com>
2021-07-18 20:43:22 +0900

    feat: main 함수 update

--- BankManager_concurrency/BankManager_concurrency/main.swift
+++ BankManager_concurrency/BankManager_concurrency/main.swift
@@ -13,21 +13,27 @@ import Foundation
         //각 클래스에 teller의 줄을 만들어주기위해서는 init에 그냥 각자에 할당할수 있는 줄을 만들어주면된다.
         //그리고 그 queue를 한번에 dispatchgroup에 묶어준다.
         //gcd로 어떻게해야할까?
+        let bank: Bank
+        let tellers: Tellers
+        var isContinue = true
         
-        let tellers = Tellers(tellerNumber: 3).list
-        
-        let windows = DispatchGroup()
-        var client = 1
-        while client <= clientCount {
-            for teller in tellers {
-                if teller.isWorking {
-                    continue
-                }
-                teller.handleBusiness(for: client, with: windows)
-                client += 1
+        while isContinue{
+            DashBoard.printMenu()
+            let command = readLine()
+            guard let command = command else {
+                print("은행 오늘 쉬는날!")
+                return
+            }
+            switch command {
+            case "\(Menu.start)":
+                let clients: Clients
+                bank.open(teller: tellers.list, client: clients.list)
+            case "\(Menu.end)":
+                isContinue = false
+            default:
+                continue
             }
         }
-        
         
     }
 
