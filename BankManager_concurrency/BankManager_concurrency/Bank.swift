8acbcbd2e41786e7f6c13ef02d234464ee2464cc
DOHYUN <pastapeter@naver.com>
2021-07-18 17:41:24 +0900

    Add: Message, Bank

---
+++ BankManager_concurrency/BankManager_concurrency/Bank.swift
@@ -0,0 +1,8 @@
+//
+//  Bank.swift
+//  BankManager_concurrency
+//
+//  Created by abc on 2021/07/18.
+//
+
+import Foundation
---
+++ BankManager_concurrency/BankManager_concurrency/Message.swift
@@ -0,0 +1,8 @@
+//
+//  Message.swift
+//  BankManager_concurrency
+//
+//  Created by abc on 2021/07/18.
+//
+
+import Foundation
