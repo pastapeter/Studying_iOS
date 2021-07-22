25599baf33f1f80e2b4cd49a45c9a1734d75ed36
DOHYUN <pastapeter@naver.com>
2021-07-23 00:48:11 +0900

    FireBase_chat

---
+++ Firebase 사용/FlashChat/Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LoginViewController.swift
+//  Flash Chat iOS13
+//
+//  Created by Angela Yu on 21/10/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+
+    @IBAction func loginPressed(_ sender: UIButton) {
+        guard let email = emailTextfield.text else {
+            return
+        }
+        guard let password = passwordTextfield.text else {
+            return
+        }
+        Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+            if let e = error {
+                print(e.localizedDescription)
+            } else {
+                self.performSegue(withIdentifier: K.loginSegue, sender: self)
+            }
+          // ...
+        }
+        
+    }
+    
+}
---
+++ Firebase 사용/FlashChat/Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RegisterViewController.swift
+//  Flash Chat iOS13
+//
+//  Created by Angela Yu on 21/10/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+    @IBAction func registerPressed(_ sender: UIButton) {
+        
+        guard let email = emailTextfield.text else {
+            return
+        }
+        guard let password = passwordTextfield.text else {
+            return
+        }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                //navigate to chatviewcontroller
+                self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+            }
+        }
+    }
+}
