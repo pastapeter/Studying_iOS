e4d2a0ad035ca3d689d696f546d158f306dec2ed
DOHYUN <pastapeter@naver.com>
2021-07-23 00:30:08 +0900

    how to Networking 1

--- NetworkingStudy/NetworkingStudy/DataTaskViewController.swift
+++ NetworkingStudy/NetworkingStudy/DataTaskViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class DataTaskViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var list = [Book]()
 
     @IBAction private func sendRequest(_ sender: Any){
         let booksUrlStr = "https://kxcoding-study.azurewebsites.net/api/books"
@@ -15,6 +19,55 @@ class DataTaskViewController: UIViewController {
         guard let url = URL(string: booksUrlStr) else {
             fatalError("invalid URL")
         }
+        
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                print("\(URLError.invalidResponse)")
+                return
+            }
+            guard (200...299).contains(httpResponse.statusCode) else {
+                print("\(httpResponse.statusCode)")
+                return
+            }
+            
+            guard let data = data else {
+                fatalError("\(URLError.invalidData)")
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .custom({ (decoder) -> Date in
+                    let container = try decoder.singleValueContainer()
+                    let dateStr = try container.decode(String.self)
+                    
+                    let formatter = ISO8601DateFormatter()
+                    formatter.formatOptions = [.withFullDate, .withTime, .withDashSeparatorInDate, .withColonSeparatorInTime]
+                    
+                    return formatter.date(from: dateStr)!
+                })
+                let booklist = try decoder.decode(BookList.self, from: data)
+                
+                if booklist.code == 200 {
+                    self.list = booklist.list
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
+                }
+            } catch {
+                print(error)
+            }
+            
+        }
+        task.resume()
+        
+        
     }
     
     override func viewDidLoad() {
@@ -25,3 +78,24 @@ class DataTaskViewController: UIViewController {
 
 }
 
+extension DataTaskViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let target = list[indexPath.row]
+        cell.textLabel?.text = target.title
+        
+        
+        return cell
+    }
+    
+    
+}
+
+extension DataTaskViewController: UITableViewDelegate {
+    
+}
